b27452ebc1df75a0ce501db246fd79606bac3ec3
Filip Skacanyi <filip.skacanyi@gmail.com>
2018-04-09 16:02:14 +0100

    created working configuration for arcade machine

--- Direct3D12 Win32 Game/InputSystem.cpp
+++ Direct3D12 Win32 Game/InputSystem.cpp
@@ -6,6 +6,115 @@
 void InputSystem::getAction(int _player, Keyboard::State _state, Keyboard::State _prev_state
 	, GameActions& _actions)
 {
+#if _ARCADE
+	switch (_player)
+	{
+	case 1:
+	{
+		if (_state.Up && !_prev_state.Up)
+		{
+			_actions.push_back(P_JUMP);
+		}
+		if (_state.Left)
+		{
+			_actions.push_back(P_MOVE_LEFT);
+		}
+		if (_state.Down)
+		{
+			_actions.push_back(P_CROUCH);
+		}
+		if (_state.Right)
+		{
+			_actions.push_back(P_MOVE_RIGHT);
+		}
+
+		if (_state.LeftControl)
+		{
+			_actions.push_back(P_HOLD_BASIC);
+		}
+		else if (_prev_state.LeftControl)
+		{
+			_actions.push_back(P_RELEASE_BASIC);
+		}
+		if (_state.LeftShift)
+		{
+			_actions.push_back(P_HOLD_SPECIAL);
+		}
+		else if (_prev_state.LeftShift)
+		{
+			_actions.push_back(P_RELEASE_SPECIAL);
+		}
+
+		if (_state.H && !_prev_state.H)
+		{
+			_actions.push_back(P_GRAB);
+		}
+		if (_state.J && !_prev_state.J)
+		{
+			_actions.push_back(P_GUARD);
+		}
+		if (_state.LeftAlt && !_prev_state.LeftAlt)
+		{
+			_actions.push_back(P_PICK_UP);
+		}
+		break;
+	}
+	case 2:
+	{
+		if (_state.R && !_prev_state.R)
+		{
+			_actions.push_back(P_JUMP);
+		}
+		if (_state.D)
+		{
+			_actions.push_back(P_MOVE_LEFT);
+		}
+		if (_state.F)
+		{
+			_actions.push_back(P_CROUCH);
+		}
+		if (_state.G)
+		{
+			_actions.push_back(P_MOVE_RIGHT);
+		}
+
+		if (_state.A)
+		{
+			_actions.push_back(P_HOLD_BASIC);
+		}
+		else if (_prev_state.A)
+		{
+			_actions.push_back(P_RELEASE_BASIC);
+		}
+		if (_state.W)
+		{
+			_actions.push_back(P_HOLD_SPECIAL);
+		}
+		else if (_prev_state.W)
+		{
+			_actions.push_back(P_RELEASE_SPECIAL);
+		}
+
+		if (_state.NumPad2 && !_prev_state.NumPad2)
+		{
+			_actions.push_back(P_GRAB);
+		}
+		if (_state.NumPad3 && !_prev_state.NumPad3)
+		{
+			_actions.push_back(P_GUARD);
+		}
+		if (_state.S && !_prev_state.S)
+		{
+			_actions.push_back(P_PICK_UP);
+		}
+		break;
+	}
+}
+	if (_state.Escape)
+	{
+		_actions.push_back(P_QUIT);
+	}
+#else
 	switch (_player)
 	{
 	case 1:
@@ -113,6 +222,8 @@ void InputSystem::getAction(int _player, Keyboard::State _state, Keyboard::State
 	{
 		_actions.push_back(P_QUIT);
 	}
+#endif
+	
 }
 
 //controller in game controls
@@ -180,6 +291,81 @@ void InputSystem::getAction(GamePad::State _state,
 //keyboard menu controls
 MenuAction InputSystem::getAction(int _player, Keyboard::State _state, Keyboard::State _prev_state)
 {
+#if _ARCADE
+	switch (_player)
+	{
+	case 1:
+	{
+		if (_state.Up && !_prev_state.Up)
+		{
+			return NAV_UP;
+		}
+		if (_state.Left && !_prev_state.Left)
+		{
+			return NAV_LEFT;
+		}
+		if (_state.Down && !_prev_state.Down)
+		{
+			return NAV_DOWN;
+		}
+		if (_state.Right && !_prev_state.Right)
+		{
+			return (NAV_RIGHT);
+		}
+
+		if (_state.LeftControl && !_prev_state.LeftControl)
+		{
+			return (CONFIRM);
+		}
+		if (_state.LeftShift && !_prev_state.LeftShift)
+		{
+			return PREVIOUS_MENU;
+		}
+		break;
+	}
+	case 2:
+	{
+		if (_state.R && !_prev_state.R)
+		{
+			return (NAV_UP);
+		}
+		if (_state.D && !_prev_state.D)
+		{
+			return (NAV_LEFT);
+		}
+		if (_state.F && !_prev_state.F)
+		{
+			return (NAV_DOWN);
+		}
+		if (_state.G && !_prev_state.G)
+		{
+			return (NAV_RIGHT);
+		}
+
+		if (_state.A && !_prev_state.A)
+		{
+			return (CONFIRM);
+		}
+		if (_state.W && !_prev_state.W)
+		{
+			return PREVIOUS_MENU;
+		}
+		break;
+	}
+	default:
+		break;
+	}
+	if (_state.Space && !_prev_state.Space)
+	{
+		return ADVANCE_MENU;
+	}
+	if (_state.Escape && !_prev_state.Escape)
+	{
+		return (QUIT);
+	}
+
+	return NONE;
+#else
 	switch (_player)
 	{
 	case 1:
@@ -252,6 +438,8 @@ MenuAction InputSystem::getAction(int _player, Keyboard::State _state, Keyboard:
 		return (QUIT);
 	}
 
+	return NONE;
+#endif
 	return NONE;
 }
 
